501e9eb69882624cc71ad74936475c87ebf2ba84
leleh5 <lelirodri50@gmail.com>
2023-09-11 21:05:22 -0300

    TP

---
+++ TP5/TP5/TP5/Emprestimo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP5
+{
+    public class Emprestimo
+    {
+        public DateTime dtEmprestimo { get; set; }
+        public DateTime dtDevolucao { get; set; }
+
+        public Emprestimo(DateTime _dtEmprestimo)
+        {
+            dtEmprestimo = _dtEmprestimo;
+        }
+
+        public Emprestimo(DateTime _dtEmprestimo, DateTime _dtDevolucao)
+        {
+            dtEmprestimo = _dtEmprestimo;
+            dtDevolucao = _dtDevolucao;
+        }
+    }
+}
---
+++ TP5/TP5/TP5/Exemplar.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP5
+{
+    public class Exemplar
+    {
+        public int tombo { get; set; }
+        public List<Emprestimo> emprestimos { get; }
+
+        public Exemplar(int _tombo)
+        {
+            tombo = _tombo;
+            emprestimos = new List<Emprestimo>();
+        }
+
+        public bool disponivel()
+        {
+            if (emprestimos.Count == 0 || emprestimos[emprestimos.Count - 1].dtDevolucao == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool emprestar()
+        {
+            if (disponivel())
+            {
+                Emprestimo data_atual = new Emprestimo(DateTime.Now);
+                emprestimos.Add(data_atual);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool devolver()
+        {
+            if (disponivel())
+            {
+                return false;
+            }
+            else
+            {
+                emprestimos[emprestimos.Count - 1].dtDevolucao = DateTime.Now;
+                return true;
+            }
+        }
+
+        public int qtdeEmprestimos()
+        {
+            return emprestimos.Count;
+        }
+    }
+}
