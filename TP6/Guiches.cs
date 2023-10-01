0c3be73f24c4bc38d48098ed9522d815075093b9
leticiarosa <38947588+leleh5@users.noreply.github.com>
2023-10-01 19:15:26 -0300

    Add files via upload

---
+++ TP6/Guiches.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP6
+{
+    public class Guiches
+    {
+        public List<Guiche> listaCuiches { get; }
+
+        public Guiches()
+        {
+            listaCuiches = new List<Guiche>();
+        }
+
+        public void adicionar(Guiche guiche)
+        {
+            listaCuiches.Add(guiche);
+        }
+    }
+}
---
+++ TP6/Senha.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP6
+{
+    public class Senha
+    {
+        public int Id { get; set; }
+        public DateTime DataGerac { get; set; }
+        public DateTime? DataAtend { get; set; }
+
+        public Senha(int id)
+        {
+            Id = id;
+            DataGerac = DateTime.Now;
+            DataAtend = null;
+        }
+
+        public string dadosParciais()
+        {
+            return Id + " - " + DataGerac.ToString("yyyy/MM/dd HH:mm:ss");
+        }
+        public string dadosCompletos()
+        {
+            return Id + " - " + DataGerac.ToString("yyyy/MM/dd HH:mm:ss") + " - " + DataAtend?.ToString("yyyy/MM/dd HH:mm:ss");
+        }
+        public void atender()
+        {
+            DataAtend = DateTime.Now;
+        }
+    }
+}
